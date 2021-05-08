1a70dbc62ad7f8eff6e5b2ce63e3449e05a4b04e
Habibur Rahman <habiburrahman-mu@users.noreply.github.com>
2021-05-09 00:07:31 +0600

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+class Hello:
+	def __init__(self, name):
+		self.a = 10
+		self.__b = 20
+		self.__c = 30
+
+	def __private_method(self): # private method
+		print('private')
+
+	def public_method(self):
+		print(self.__c)
+		print('public')
+		hello.__private_method()
+
+hello = Hello('name')
+hello.public_method()
